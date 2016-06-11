2d8cd13190fdfcb3ac99656423d6998962c899c4
Liam Kirsher <liamk@numenet.com>
2016-06-11 16:49:53 -0700

    Removing threeway

--- threeway/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import *
-from django.conf.urls import url, patterns, include
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    url(r'^$',
-        TemplateView.as_view(template_name='threeway/index.html'),
-        name='threeway'),
-]
